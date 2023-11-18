83420ff31c57c5dd4cfdb75c9381ef17846af207
Amirali Danai <danaiamirali@gmail.com>
2023-11-18 18:19:08 -0500

    initial changes

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ app.secret_key = 'resume_screener'
 socketio = SocketIO(app, async_mode = 'eventlet')
 
 filename = ""
+str = ""
+
 from modules.screener import Screener
 
 def highlight():
@@ -24,6 +26,43 @@ def highlight():
 @socketio.on('connect', namespace='/upload')
 def handle_connect():
     #highlight()
+    global str
+    job_desc = """
+    Below are the skills needed for this project. Students with the following relevant skills and interest, regardless of major, are encouraged to apply! This is a team based multidisciplinary project. Students on the team are not expected to have experience in all areas, but should be willing to learn and will be asked to perform a breadth of tasks throughout the two semester project.
+    Advanced Data Science and Modeling Techniques
+    Specific Skills: Applied project experience with Large Language Models and other applied AI techniques OR advanced coursework
+
+    Likely Majors: DATA, STATS, MATH, CS
+
+
+    Data Science
+    Specific Skills: General skills in Data Science, good software development skills, and a willingness to quickly develop new technical skills as required for the project
+
+    EECS 281(or equivalent) is required
+
+    Likely Majors: DATA, CS
+
+
+    General Coding
+    Specific Skills: General Programming skills, good software engineering practice and design, and a willingness to quickly develop new technical skills as required for the project 
+
+    EECS 281 (or equivalent) is required
+
+    Likely Majors: CS, DATA, BBA/CS
+
+
+
+    Additional Desired Skills/Knowledge/Experience
+    Successful team-based project experience.  Excellent interpersonal skills
+    Project Management utilizing Agile/Scrum
+    Experience in business process analysis
+    Interest in and general knowledge of Commercial Banking
+    Practical experience implementing predictive analytics in a complex data environment
+    Ability and desire to independently learn new technology skills as necessary for the project
+    Experience implementing large language models, neural networks and self-supervised / semi-supervised learning models
+    """
+    s = Screener("static/resume.pdf", job_desc)
+    str = s.is_correct_fit()
     socketio.emit('redirect', namespace='/upload')
     # do something
 
@@ -48,7 +87,8 @@ def upload():
 
 @app.route("/results", methods=["POST", "GET"])
 def results():
-    return render_template("result.html")
+    global str
+    return render_template("result.html", str=str)
     
 
 
