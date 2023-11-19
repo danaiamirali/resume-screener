a6524fbafe879e4da6ec08f42ca5334692fc0294
TiernanJesrani <134875734+TiernanJesrani@users.noreply.github.com>
2023-11-18 23:12:53 -0500

    push html and text

--- bestmatches.py
+++ bestmatches.py
@@ -1,27 +1,32 @@
 import requests
 
-api_key = "c7c24d4a2e29cda5e041c25566488f571a48c02e5a9c8e5c5ed75748de4f8350"
+def Match():
 
-url = "https://www.themuse.com/api/public/jobs"
+    api_key = "c7c24d4a2e29cda5e041c25566488f571a48c02e5a9c8e5c5ed75748de4f8350"
 
-params = {"api_key": api_key, "level": "Internship", "page": 1}
+    url = "https://www.themuse.com/api/public/jobs"
 
-response = requests.get(url, params=params)
+    
 
-if response.status_code == 200:
-    # Parse the JSON data from the response
-    try:
-        data = response.json()
+    params = {"api_key": api_key, "level": "Internship", "page": 1}
 
-        # Now 'data' contains the information returned by the API
-        # You can process and use this data as needed
-        for job in data.get('results', []):
-            print(f"Job Title: {job.get('name')}")
-            print(f"Company: {job.get('company', {}).get('name')}")
-            print(f"Location: {job.get('locations', [])[0].get('name')}")
-            print("-----")
+    response = requests.get(url, params=params)
 
-    except ValueError as e:
-        print(f"Error parsing JSON: {e}")
+    if response.status_code == 200:
+        # Parse the JSON data from the response
+        try:
+            data = response.json()
+
+            # Now 'data' contains the information returned by the API
+            # You can process and use this data as needed
+            for job in data.get('results', []):
+                print(f"Job Title: {job.get('name')}")
+                print(f"Company: {job.get('company', {}).get('name')}")
+                print(f"Location: {job.get('locations', [])[0].get('name')}")
+                print("-----")
+
+        except ValueError as e:
+            print(f"Error parsing JSON: {e}")
+
 
 
