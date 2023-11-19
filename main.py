c3a22eda9ad7c3937f0b5b4929772acc535c8356
Amirali Danai <danaiamirali@gmail.com>
2023-11-19 00:07:15 -0500

    Attempted main integration

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from fitz.utils import getColor
 import fitz
 import os
 
+from threading import Thread
 
 app = Flask(__name__, static_folder="static")
 app.secret_key = 'resume_screener'
 socketio = SocketIO(app, async_mode = 'eventlet')
 
 filename = ""
+job = ""
 str = ""
 
 from modules.screener import Screener
@@ -27,9 +29,6 @@ def highlight(hi_list):
         highlight = page.add_highlight_annot(text_instances)
         highlight.set_colors(stroke=color)
         highlight.update()
-
-    #text_instances = page.search_for("Tiernan")      
-    #page.add_highlight_annot(text_instances)
     
     file1 = file[:-4]
     file1 = file1 + '1.pdf'
@@ -41,7 +40,40 @@ def highlight(hi_list):
 
 @socketio.on('connect', namespace='/upload')
 def handle_connect():
-    hi_list = []
+    global filename
+    global job
+
+    s = Screener(filename, job)
+    strengths = None
+    weaknesses = None
+
+    def init_strengths():
+        print("running strengths...")
+        strengths = s.strengths()
+        print("strengths: ", strengths)
+
+    def init_weaknesses():
+        print("running weaknesses...")
+        weaknesses = s.weaknesses()
+        print("weaknesses: ", weaknesses)
+
+    # strength_thread = Thread(target = init_strengths)
+    # weakness_thread = Thread(target = init_weaknesses)
+    # strength_thread.start()
+    # weakness_thread.start()
+    # strength_thread.join()
+    # weakness_thread.join()
+
+    # strengths = eval(strengths)
+    # weaknesses = eval(weaknesses)
+
+    hi_list = []   
+
+    for i in strengths:
+        hi_list.append((i[0], "green"))
+    for i in weaknesses:
+        hi_list.append((i[0], "red"))
+
     highlight(hi_list)
     socketio.emit('redirect', namespace='/upload')
     # do something
@@ -53,9 +85,10 @@ def home():
             return render_template("home.html")
         else:
             global filename
+            global job
             job = request.form['job']
             resume = request.files['resume']
-            filename = "../static/" + resume.filename
+            filename = "static/" + resume.filename
             resume.save('static/' + resume.filename)
             return redirect(url_for("upload"))
     else:
