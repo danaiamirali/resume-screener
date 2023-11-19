01362793655e1d0814dc46c35ed2159593bfe188
Amirali Danai <danaiamirali@gmail.com>
2023-11-18 19:27:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -41,49 +41,8 @@ def highlight(hi_list):
 
 @socketio.on('connect', namespace='/upload')
 def handle_connect():
-<<<<<<< Updated upstream
-    #highlight()
-    global str
-    job_desc = """
-    Below are the skills needed for this project. Students with the following relevant skills and interest, regardless of major, are encouraged to apply! This is a team based multidisciplinary project. Students on the team are not expected to have experience in all areas, but should be willing to learn and will be asked to perform a breadth of tasks throughout the two semester project.
-    Advanced Data Science and Modeling Techniques
-    Specific Skills: Applied project experience with Large Language Models and other applied AI techniques OR advanced coursework
-
-    Likely Majors: DATA, STATS, MATH, CS
-
-
-    Data Science
-    Specific Skills: General skills in Data Science, good software development skills, and a willingness to quickly develop new technical skills as required for the project
-
-    EECS 281(or equivalent) is required
-
-    Likely Majors: DATA, CS
-
-
-    General Coding
-    Specific Skills: General Programming skills, good software engineering practice and design, and a willingness to quickly develop new technical skills as required for the project 
-
-    EECS 281 (or equivalent) is required
-
-    Likely Majors: CS, DATA, BBA/CS
-
-
-
-    Additional Desired Skills/Knowledge/Experience
-    Successful team-based project experience.  Excellent interpersonal skills
-    Project Management utilizing Agile/Scrum
-    Experience in business process analysis
-    Interest in and general knowledge of Commercial Banking
-    Practical experience implementing predictive analytics in a complex data environment
-    Ability and desire to independently learn new technology skills as necessary for the project
-    Experience implementing large language models, neural networks and self-supervised / semi-supervised learning models
-    """
-    s = Screener("static/resume.pdf", job_desc)
-    str = s.is_correct_fit()
-=======
     hi_list = []
     highlight(hi_list)
->>>>>>> Stashed changes
     socketio.emit('redirect', namespace='/upload')
     # do something
 
@@ -108,15 +67,7 @@ def upload():
 
 @app.route("/results", methods=["POST", "GET"])
 def results():
-<<<<<<< Updated upstream
-    global str
-    return render_template("result.html", str=str)
-=======
     return render_template("result.html", path=filename)
->>>>>>> Stashed changes
-    
-
-
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
